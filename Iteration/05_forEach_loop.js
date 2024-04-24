8644c150249996cb84512375f838cfad3806b2d1
Harsh Mehra <me.harshmehra07@gmail.com>
2024-04-24 22:38:13 +0530

    up

---
+++ Iteration/05_forEach_loop.js
@@ -0,0 +1,10 @@
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNums = myNums.filter((num) => {
+    return num > 5
+})
+
+console.log(newNums);
+
+// The main difference b/w "forEach loop" and "filter loop" is "filter loop" returns value and "for each loop" do not return value  
