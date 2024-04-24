4518e963f0afc786d55fc7700ffae86ca6cfd266
Harsh Mehra <me.harshmehra07@gmail.com>
2024-04-24 22:12:17 +0530

    up

--- Iteration/03_highOrder_loops.js
+++ Iteration/03_highOrder_loops.js
@@ -1,6 +1,6 @@
 // High order Array Loops
 
-// "for of" loop
+// "for of" loop cannot be applied on objects 
 const val = [1, 2, 3, 4]
 for (const num of val) {
     console.log(num);
@@ -16,4 +16,40 @@ for (const greet of greeting) {
 console.log(typeof result);
 console.log(result);
 
+// Iterating over map
+const map = new Map()
+map.set('IN', 'India')
+map.set('USA', 'United States')
+map.set('Fr', 'France')
+map.set('IN', "India")
 
+console.log(map);   // output: Map(3) { 'IN' => 'India', 'USA' => 'United States', 'Fr' => 'France' }
+
+for (const [key, val] of map) {
+    console.log(key, ':-', val);
+    
+}
+/* output: IN :- India
+           USA :- United States
+           Fr :- France */
+
+
+// "for in" loop on objects
+const myObj = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+} 
+
+for (const key in myObj) {
+    console.log(`${key} shortcut is for ${myObj[key]}`);
+    
+}
+
+// "for in" loop on Array
+const lang = ['Java', 'C++', 'Dart', 'JS']
+for (const key in lang) {
+    console.log(`${key} is for ${lang[key]}`);
+    
+}
