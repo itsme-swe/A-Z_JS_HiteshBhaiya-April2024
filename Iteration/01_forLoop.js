5c58b5503f9d048b8fd31e0c61b294d992461c1f
Harsh Mehra <me.harshmehra07@gmail.com>
2024-04-24 19:54:52 +0530

    up

--- Iteration/01_forLoop.js
+++ Iteration/01_forLoop.js
@@ -4,3 +4,21 @@ for (let i = 0; i < 10; i++) {
     const element = i
     console.log(element);
 }
+
+let myCars = ['Panamera', 'Aston Martin', 'BMW', 'Bentley']
+
+for (let i = 0; i < myCars.length; i++){
+    const element = myCars[i]
+    console.log(element);
+}
+
+// break and continue 
+
+for (let i = 1; i <= 20; i++){
+    if (i == 5){
+        console.log(`Detected 5`);
+        break
+    }
+    console.log(`Value of i is ${i}`);
+    
+}
---
+++ Iteration/02_whileLoop.js
@@ -0,0 +1,17 @@
+let i = 1
+let result = ""
+while (i <= 10){
+    result += i +  " ";
+    i++;
+}
+console.log(result);
+
+// Using while loop on Array
+
+let myCollection = ['Bhakti', 'Home', 'Aston Martin', 'Laptop']
+
+let arr = 0
+while (arr < myCollection.length){
+    console.log(myCollection[arr]);
+    arr++
+}
