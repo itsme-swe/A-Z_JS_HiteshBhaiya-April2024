4622720f7689374b71dc2d4d30eaf3d69cb4ed45
Harsh Mehra <me.harshmehra07@gmail.com>
2024-04-24 21:51:14 +0530

    up

--- Iteration/02_whileLoop.js
+++ Iteration/02_whileLoop.js
@@ -15,3 +15,12 @@ while (arr < myCollection.length){
     console.log(myCollection[arr]);
     arr++
 }
+
+
+// do while loop
+
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++
+}while(score <= 10)
---
+++ Iteration/03_highOrder_loops.js
@@ -0,0 +1,19 @@
+// High order Array Loops
+
+// "for of" loop
+const val = [1, 2, 3, 4]
+for (const num of val) {
+    console.log(num);
+    
+}
+
+const greeting = "Hello World"
+let result = ""
+for (const greet of greeting) {
+    result += greet + ""
+    
+}
+console.log(typeof result);
+console.log(result);
+
+
