d14bdd99364961767cd2dab23bcd4d8d9cc480a8
Harsh Mehra <me.harshmehra07@gmail.com>
2024-04-25 17:46:41 +0530

    upda

--- Advance_Topics_2/02_promise.js
+++ Advance_Topics_2/02_promise.js
@@ -10,3 +10,22 @@ promiseThree.then((user) => {
     console.log(user);      // Data Consumption when passed as Parameter inside 'resolve()'
     
 })
+
+console.log();
+
+// Creatig new Promise
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true            // This time now we'll be handling error
+
+        // Here if condition get's true the resolve statement will execute other wise the reject statement will execute
+        if (!error){
+            resolve({carName: 'Aston Martin', ownerName: 'Harsh', color: 'glossy green'})
+        } else{
+            reject('ERROR: Something went wrong')
+        }
+    }, 3000)
+}).then(function(user){
+    console.log(user);
+    
+})
